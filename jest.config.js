5efb5508d37b14810bc76ec6aa2c86343895f49f
Brandon Carroll <brandonvcarroll@gmail.com>
2019-04-28 18:02:17 -0400

    don't collect coverage from preset

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,10 @@ const jestPreset = require('react-native/jest-preset');
 const ignores = ['/node_modules/', '/__tests__/helpers/', '__mocks__'];
 
 module.exports = Object.assign(jestPreset, {
-  transformIgnorePatterns: ['node_modules/(?!(react-native.*|@?react-navigation.*)/)'],
+  collectCoverageFrom: ['**/src/lib/**/*.js', '!**/src/preset/**/*.js'],
   setupFiles: [...jestPreset.setupFiles, require.resolve('./src/preset/setup.js')],
   testPathIgnorePatterns: [...ignores],
+  transformIgnorePatterns: ['node_modules/(?!(react-native.*|@?react-navigation.*)/)'],
   coverageThreshold: {
     global: {
       branches: 100,
