e81488406fe5ca6b15ce72479ba0124445aee42f
Brandon Carroll <brandonvcarroll@gmail.com>
2019-04-28 17:42:54 -0400

    fix tests in ci

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
-const jestPreset = require('./jest-preset');
+const jestPreset = require('react-native/jest-preset');
 
 const ignores = ['/node_modules/', '/__tests__/helpers/', '__mocks__'];
 
 module.exports = Object.assign(jestPreset, {
-  collectCoverageFrom: ['src/lib/**/*.+(js|jsx|ts|tsx)'],
+  transformIgnorePatterns: ['node_modules/(?!(react-native.*|@?react-navigation.*)/)'],
+  setupFiles: [...jestPreset.setupFiles, require.resolve('./src/preset/setup.js')],
   testPathIgnorePatterns: [...ignores],
   coverageThreshold: {
     global: {
