4233b0529396c0844b28197b9fc281a57ea53035
Mateusz Mędrek <luigimario1913@gmail.com>
2019-06-29 15:44:34 +0200

    fix: removed old ByA11yStates and ByA11yTraits typings, added ByDisplayValue and ByTitle typings (#40)

--- typings/queries.d.ts
+++ typings/queries.d.ts
@@ -65,56 +65,56 @@ export type FindByText = (
   waitForElementOptions?: WaitForElementOptions,
 ) => Promise<NativeTestInstance>;
 
+export const getByDisplayValue: GetByBoundProp;
 export const getByHintText: GetByBoundProp;
 export const getByLabelText: GetByBoundProp;
 export const getByRole: GetByBoundProp;
-export const getByA11yStates: GetByBoundProp;
-export const getByA11yTraits: GetByBoundProp;
 export const getByPlaceholderText: GetByBoundProp;
 export const getByTestId: GetByBoundProp;
 export const getByText: GetByText;
+export const getByTitle: GetByBoundProp;
 
+export const getAllByDisplayValue: AllByBoundProp;
 export const getAllByHintText: AllByBoundProp;
 export const getAllByLabelText: AllByBoundProp;
 export const getAllByRole: AllByBoundProp;
-export const getAllByA11yStates: AllByBoundProp;
-export const getAllByA11yTraits: AllByBoundProp;
 export const getAllByPlaceholderText: AllByBoundProp;
 export const getAllByTestId: AllByBoundProp;
 export const getAllByText: AllByText;
+export const getAllByTitle: AllByBoundProp;
 
+export const queryByDisplayValue: QueryByBoundProp;
 export const queryByHintText: QueryByBoundProp;
 export const queryByLabelText: QueryByBoundProp;
 export const queryByRole: QueryByBoundProp;
-export const queryByA11yStates: QueryByBoundProp;
-export const queryByA11yTraits: QueryByBoundProp;
 export const queryByPlaceholderText: QueryByBoundProp;
 export const queryByTestId: QueryByBoundProp;
 export const queryByText: QueryByText;
+export const queryByTitle: QueryByBoundProp;
 
+export const queryAllByDisplayValue: AllByBoundProp;
 export const queryAllByHintText: AllByBoundProp;
 export const queryAllByLabelText: AllByBoundProp;
 export const queryAllByRole: AllByBoundProp;
-export const queryAllByA11yStates: AllByBoundProp;
-export const queryAllByA11yTraits: AllByBoundProp;
 export const queryAllByPlaceholderText: AllByBoundProp;
 export const queryAllByTestId: AllByBoundProp;
 export const queryAllByText: AllByText;
+export const queryAllByTitle: AllByBoundProp;
 
+export const findByDisplayValue: FindByBoundProp;
 export const findByHintText: FindByBoundProp;
 export const findByLabelText: FindByBoundProp;
 export const findByRole: FindByBoundProp;
-export const findByA11yStates: FindByBoundProp;
-export const findByA11yTraits: FindByBoundProp;
 export const findByPlaceholderText: FindByBoundProp;
 export const findByTestId: FindByBoundProp;
 export const findByText: FindByText;
+export const findByTitle: FindByBoundProp;
 
+export const findAllByDisplayValue: FindAllByBoundProp;
 export const findAllByHintText: FindAllByBoundProp;
 export const findAllByLabelText: FindAllByBoundProp;
 export const findAllByRole: FindAllByBoundProp;
-export const findAllByA11yStates: FindAllByBoundProp;
-export const findAllByA11yTraits: FindAllByBoundProp;
 export const findAllByPlaceholderText: FindAllByBoundProp;
 export const findAllByTestId: FindAllByBoundProp;
 export const findAllByText: FindAllByText;
+export const findAllByTitle: FindAllByBoundProp;
