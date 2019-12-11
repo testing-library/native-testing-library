a1f9f85904a7fda34ab7ff3e1228faa9000bf0cd
Brandon Carroll <bcarroll22@users.noreply.github.com>
2019-12-11 11:42:30 -0500

    fix(revert): upgrade to latest react-navigation v4 (#94)
    This reverts commit 065e5f8ad48f3d6a004f94d28dc55c21c2077c4b.

--- examples/__tests__/react-navigation.js
+++ examples/__tests__/react-navigation.js
@@ -1,8 +1,7 @@
 import '@testing-library/jest-native/extend-expect';
 import React from 'react';
 import { Button, Text, View } from 'react-native';
-import { createAppContainer, withNavigation } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import { createStackNavigator, createAppContainer, withNavigation } from 'react-navigation';
 
 import { render, fireEvent } from '../../src';
 
@@ -15,45 +14,6 @@ jest
       PanGestureHandler: View,
       BaseButton: View,
       Directions: {},
-    };
-  })
-  .mock('react-native-reanimated', () => {
-    const View = require('react-native').View;
-
-    const Easing = {
-      linear: jest.fn(),
-      ease: jest.fn(),
-      quad: jest.fn(),
-      cubic: jest.fn(),
-      poly: jest.fn(),
-      sin: jest.fn(),
-      circle: jest.fn(),
-      exp: jest.fn(),
-      elastic: jest.fn(),
-      back: jest.fn(),
-      bounce: jest.fn(),
-      bezier: jest.fn(),
-      in: jest.fn(),
-      out: jest.fn(),
-      inOut: jest.fn(),
-    };
-
-    return {
-      Easing,
-      Value: jest.fn(),
-      event: jest.fn(),
-      add: jest.fn(),
-      eq: jest.fn(),
-      set: jest.fn(),
-      cond: jest.fn(),
-      interpolate: jest.fn(),
-      View: View,
-      Extrapolate: { CLAMP: jest.fn() },
-      Transition: {
-        Together: 'Together',
-        Out: 'Out',
-        In: 'In',
-      },
     };
   });
 
--- jest-preset.js
+++ jest-preset.js
@@ -3,7 +3,7 @@ const jestPreset = require('react-native/jest-preset');
 module.exports = Object.assign(jestPreset, {
   transformIgnorePatterns: [
     ...jestPreset.transformIgnorePatterns,
-    'node_modules/(?!(react-native.*|@?react-navigation.*|@?react-native-community.*)/)',
+    'node_modules/(?!(react-native.*|@?react-navigation.*)/)',
   ],
   snapshotSerializers: [require.resolve('./dist/preset/serializer.js')],
   setupFiles: [...jestPreset.setupFiles, require.resolve('./dist/preset/setup.js')],
